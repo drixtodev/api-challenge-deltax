b80c77082c3ce29de06f8f7031e5ce2793748581
Jesus Fernandez <jesusfernxndez10@gmail.com>
2022-07-23 15:14:29 -0500

    add .env and config file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("./database");
+const { PORT } = require("./config");
 
 const app = express();
 
@@ -9,6 +10,6 @@ app.use(express.json());
 
 app.use("/api/v1/pets", require("./routes"));
 
-app.listen("4000", () => {
-  console.log("Server is running on port 4000");
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
